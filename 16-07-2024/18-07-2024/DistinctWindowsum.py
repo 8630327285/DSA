66c6b6edc9622f5da0a63cb3d486e1abeac6be9b
Nitinkumar <nitinkumar18119@gmail.com>
2024-07-19 10:14:18 +0530

    prefix.sum.py

---
+++ 16-07-2024/18-07-2024/DistinctWindowsum.py
@@ -0,0 +1,27 @@
+def getMaximumSubarraySumWithDistinceElemente(arr,k);
+  i = 0
+  j = k-1
+  ans = float("-inf")
+  ps = 0
+  _map = {}
+  while j < len(arr);
+    cs = 0
+    if i == 0:
+      cs = 0
+      if i == 0:
+        cs = 0
+        for k1 in range(i,j+1):
+          cs+arr[k1]
+          if arr[k1] in _map:
+            value = _map[arr[k1]]
+            value+=1
+            _map[arr[k1]]=1
+          else:
+            _map[arr[k1]]=1
+            ps = cs
+          else:
+key = arr[i-1]
+if_map[key] == 1:
+del_map[arr[i-1]]
+else:
+
